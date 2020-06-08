56d0e2d27eeaf5f35a698d3f6d8153e841be638b
mroca1 <mathias.roca@linty-services.com>
2020-06-08 17:22:08 +0200

    Update package.json
    Add missing headers
    Update readme.md

--- README.md
+++ README.md
@@ -3,8 +3,15 @@ Sonar FPGA Metrics Plugin
 
 A SonarQube plugin allowing to create custom metrics and assign them values from external files.
 
-### Building
+### Building and installing
 
 Build the plugin with :
 
 mvn clean package
+
+Copy the generated jar file from /target to "Sonarqube server path"/extensions/plugins , then launch Sonarqube.
+
+### Usage
+
+All custom metrics should be defined in a json file. The absolute path to this file can be defined in Sonarqube web interface, in the plugin's properties (Administration tab). Sonarqube server needs to be restarted when this file is modified, in order for the changes to apply.
+Measures for each custom metric should be defined in a file named measures.json at the root of the analyzed project, when they relate to a project. When measures relate to a single file, they should be in a json file named [Source file name without extension]_measures.json, in the same folder as the corresponding file. Measures will be imported in Sonarqube user interface after executing sonar-scanner. 
--- src/test/java/org/linty/plugins/fpgametrics/MeasuresImporterTest.java
+++ src/test/java/org/linty/plugins/fpgametrics/MeasuresImporterTest.java
@@ -1,3 +1,21 @@
+ /*
+ * sonar-fpga-metrics plugin for Sonarqube
+ * Copyright (C) 2020 Linty Services
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.linty.plugins.fpgametrics;
 
 
@@ -14,13 +32,12 @@ import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
 public class MeasuresImporterTest {
-    private static final String BASE_DIR = "tmp";
-
 
     
     @Test
     public void execute_WithMatchingPattern_ExpectValueRegistered() throws Exception {
-        final SensorContextTester contextTester = SensorContextTester.create(new File(BASE_DIR));
+    	
+        final SensorContextTester contextTester = SensorContextTester.create(new File("src\\test\\files\\ctx")); //Blank file only used for context simulation
 
         final MeasuresImporter mesuresImporter = new MeasuresImporter(Arrays.asList(FLOAT_METRIC, INT_METRIC, STRING_METRIC),"src\\test\\files\\");
         mesuresImporter.execute(contextTester);
